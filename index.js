167a24b226632a2ed1fdca0f6ea4d22e03a68342
andrew wong <awwong92@gmail.com>
2018-09-14 01:25:34 -0400

    removed JS file

--- index.js
+++
@@ -1,9 +0,0 @@
-const elements = document.getElementsByClassName("navigation__link");
-const navToggle = document.getElementById('navi-toggle')
-
-Array.prototype.slice.call(elements)
-  .forEach(function (el) {
-    el.addEventListener('click', function (e) {
-      navToggle.checked = false
-    })
-  })
